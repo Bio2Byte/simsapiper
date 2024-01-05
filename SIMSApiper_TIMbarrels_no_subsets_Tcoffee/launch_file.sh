d7bdad3223133f660dfcca5806f89c9c1f1d1a4a
charlieccp <74074591+charlieccp@users.noreply.github.com>
2024-01-05 14:14:13 +0100

    Update launch_file.sh

--- SIMSApiper_TIMbarrels_no_subsets_Tcoffee/launch_file.sh
+++ SIMSApiper_TIMbarrels_no_subsets_Tcoffee/launch_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load Nextflow/23.04.2
-house=$VSC_SCRATCH_VO/vsc10579/simsapiper
+house=$(pwd)
 data=SIMSApiper_TIMbarrels_no_subsets_Tcoffee
 now=`date +"%Y_%m_%d_%H_%M_%S"`
 output_name=${data}_${now}_test
