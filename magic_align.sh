da99b70d70dca7d7436ce580aa6dc17089bb1c97
vsc10611 <vsc10611@login2.cerberus.os>
2024-02-16 16:32:24 +0100

    correct launch files

--- magic_align.sh
+++ magic_align.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-module load Nextflow/23.04.2
-house=$VSC_SCRATCH_VO_USER/
-data=/scratch/brussel/vo/000/bvo00023/vsc10611/simsapiper/tests
+house=$(pwd)
+data=toy_example
 now=`date +"%Y_%m_%d_%H_%M_%S"`
-output_name=${now}
-output_folder=$data/results/$output_name
-mkdir -p $data/results/
+output_name=${data}_${now}_test
+output_folder=$house/results/$output_name
+mkdir -p $house/results/
 mkdir -p $output_folder
 echo 'Starting nextflow'
-nextflow run /scratch/brussel/vo/000/bvo00023/vsc10611/simsapiper/simsapiper.nf \
-    -profile hydra \
-    --data $data/data \
-    --seqQC 5 \
-    --dropSimilar 90 \
-    --createSubsets 30 \
-    --minSubsetID "min" \
-    --retrieve true \
-    --model true \
-    --strucQC 5 \
-    --reorder true \
+nextflow run simsapiper.nf \
+    -profile server,withsingularity \
+    --data $house/$data/data \
+    --magic \
     --outFolder $output_folder \
     -with-trace $output_folder/timings_$output_name.txt \
     |& tee  $output_folder/run_report_$output_name.nflog
