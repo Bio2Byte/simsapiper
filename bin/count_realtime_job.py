5e16dda2e89e239400f15086898f6e454616b23a
vsc10611 <vsc10611@login1.cerberus.os>
2024-02-28 10:21:50 +0100

    small implementation

--- bin/count_realtime_job.py
+++ bin/count_realtime_job.py
@@ -4,13 +4,20 @@ import sys
 
 def extract_realtime_column(file_path):
     realtimes = []
+    realtimes_tcoffee = []
     with open(file_path, newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
         for row in reader:
             realtimes.append(float(row['realtime']))
-    return realtimes
+            for element in list(row.values()):
+                if element.startswith("runTcoffee"):
+                    realtimes_tcoffee.append(float(row['realtime']))
+    return realtimes,realtimes_tcoffee
 
 file_path = sys.argv[1]
-realtime_columns = extract_realtime_column(file_path)
+realtime_columns,realtimes_tcoffee_columns = extract_realtime_column(file_path)
 total = sum(realtime_columns)/60000 #convert to minutes
+total_tcoffee = sum(realtimes_tcoffee_columns)/60000 #convert to minutes
+
 print(f"Total realtime: {round(total)} minutes")
+print(f"Total realtime tcoffee only: {round(total_tcoffee)} minutes")
