90bc25f0e6826b2640637e326f44b2c217624dc9
vsc10579 <vsc10579@login2.cerberus.os>
2024-03-12 13:10:20 +0100

    fix path issue

--- magic_hydra.sh
+++ magic_hydra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-module load Nextflow/23.04.2
-house=$VSC_SCRATCH/simsapiper
+module load Nextflow/23.10.0
+house=$VSC_SCRATCH_VO_USER/simsapiper
 data=toy_example
 now=`date +"%Y_%m_%d_%H_%M_%S"`
 output_name=${data}_${now}_test
@@ -8,7 +8,7 @@ output_folder=$house/results/$output_name
 mkdir -p $house/results
 mkdir -p $output_folder
 nextflow run simsapiper.nf \
-    -profile hydra,withsingularity \
+    -profile hydra \
     --data $house/$data/data \
     --magic \
     --outFolder $output_folder \
@@ -16,3 +16,5 @@ nextflow run simsapiper.nf \
     |& tee  $output_folder/run_report_$output_name.nflog
 sessionName=$(sed -n '2s/.*\[\(.*\)\].*/\1/p' $output_folder/run_report_$output_name.nflog)
 nextflow log | grep $sessionName >> $output_folder/run_report_$output_name.nflog
+
+#screen -S nextflowalign bash -c ./magic_hydra.sh
